b7b2926bd97497065eb6cd2b58d626a384d50130
iamDebe <emmanueljoce@gmail.com>
2025-02-18 10:20:22 +0100

    footer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,15 +4,15 @@ const Navbar = () => {
       <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-ftvgrey-200 dark:border-ftvgrey-600 backdrop-blur navbar">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
-            href="https://flowbite.com/"
+            href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               className="h-8"
-              alt="Flowbite Logo"
+              alt="Fasttrack VA Logo"
             />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-ftvblack-300">
-              Flowbite
+              Fasttrack VA
             </span>
           </a>
 
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -179,7 +179,7 @@ const Landing = () => {
             <div className="w-4/12 min-h-60 flex flex-col p-4  justify-evenly items-start rounded-lg ring-1 dark:ring-ftvwine-400 ring-ftvwine-400 dark:bg-ftvwine-25  bg:ftvwine-100  hover:dark:bg-white transition ease-in-out duration-300 border-t-5 border-ftvwine-500 dark:border-ftvwine-500 hover:shadow-xl">
               <h1 className="font-bold text-ftvwine-500 text-lg">
                 {" "}
-                3. Coontracts & Onboarding
+                3. Contracts & Onboarding
               </h1>
               <p className="font-normal text-ftvwine-400 text-lg">
                 Once you pick your preferred talent, we will send you our
@@ -209,6 +209,299 @@ const Landing = () => {
           </button>
         </div>
       </section>
+      <footer class="bg-ftvwine-500 antialiased dark:bg-ftvwine-500">
+        <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+          <div class="border-b border-gray-100 py-6 dark:border-ftvgrey-200 md:py-8 lg:py-16">
+            <div class="items-start gap-6 md:gap-8 lg:flex 2xl:gap-24">
+              <div class="grid min-w-0 flex-1 grid-cols-2 gap-6 md:gap-8 xl:grid-cols-3">
+                <div>
+                  <h6 class="mb-4 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                    Company
+                  </h6>
+                  <ul class="space-y-3">
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        {" "}
+                        Home{" "}
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        {" "}
+                        About{" "}
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        {" "}
+                        Services
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        {" "}
+                        Contact
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div>
+                  <h6 class="mb-4 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                    Fasttrack Virtuals
+                  </h6>
+                  <ul class="space-y-3">
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Digital Marketing
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Administrative Tasks
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Financial Management
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Payment Methods
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div>
+                  <h6 class="mb-4 text-sm font-semibold uppercase text-gray-900 dark:text-white">
+                    Support & Services
+                  </h6>
+                  <ul class="space-y-3">
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Contact Support
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        FAQs
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        title=""
+                        class="text-gray-500 hover:text-gray-900 dark:text-ftvwine-25 dark:hover:text-white">
+                        Returns & Refunds
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div class="mt-6 w-full md:mt-8 lg:mt-0 lg:max-w-lg">
+                <div class="space-y-5 rounded-lg bg-gray-50 p-6 dark:bg-ftvwine-100">
+                  <a
+                    href="#"
+                    title=""
+                    class="text-base font-medium text-ftvwine-500  hover:underline dark:text-ftvwine-500">
+                    {" "}
+                    Sign In or Create Account{" "}
+                  </a>
+
+                  <hr class="border-ftvwine-500 dark:borderftvwine-500" />
+
+                  <form action="#">
+                    <div class="items-end space-y-4 sm:flex sm:space-y-0">
+                      <div class="relative mr-3 w-full sm:w-96 lg:w-full">
+                        <label
+                          for="email"
+                          class="mb-2 block text-sm font-medium text-ftvwine-500 dark:text-ftvwine-500">
+                          {" "}
+                          Get the latest deals and more.{" "}
+                        </label>
+                        <input
+                          class="block w-full rounded-lg border border-gray-300 bg-white p-3 text-sm text-ftvblack-300 focus:border-ftvwine-300 focus:ring-primary-500 dark:border-gray-500 dark:bg-ftvgrey-100 dark:text-ftvblack-300 dark:placeholder-gray-400 dark:focus:border-ftvwine-400 dark:focus:ring-primary-500 sm:w-96 lg:w-full outline-none"
+                          placeholder="Enter your email address"
+                          type="email"
+                          id="email"
+                          required=""
+                        />
+                      </div>
+                      <div>
+                        <button
+                          type="submit"
+                          class="w-full cursor-pointer rounded-lg bg-primary-700 px-5 py-3 text-center text-sm font-medium text-white hover:bg-ftvwine-400 focus:ring-4 focus:ring-primary-300 dark:bg-ftvwine-500 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                          Subscribe
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+
+                  <hr class="border-ftvwine-500 dark:borderftvwine-500" />
+
+                  <div class="flex space-x-4">
+                    <a
+                      href="#"
+                      class="text-ftvwine-500 hover:text-ftvwine-500 dark:text-ftvwine-500 dark:hover:text-ftvwine-400">
+                      <svg
+                        class="w-6 h-6 "
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24">
+                        <path
+                          fill="currentColor"
+                          fill-rule="evenodd"
+                          d="M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </a>
+
+                    <a
+                      href="#"
+                      class="text-ftvwine-500 hover:text-ftvwine-500 dark:text-ftvwine-500 dark:hover:text-ftvwine-400">
+                      <svg
+                        class="w-6 h-6 "
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path
+                          fill-rule="evenodd"
+                          d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z"
+                          clip-rule="evenodd"
+                        />
+                        <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
+                      </svg>
+                    </a>
+
+                    <a
+                      href="#"
+                      class="text-ftvwine-500 hover:text-ftvwine-500 dark:text-ftvwine-500 dark:hover:text-ftvwine-400">
+                      <svg
+                        class="w-6 h-6 "
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
+                      </svg>
+                    </a>
+
+                    <a
+                      href="#"
+                      class="text-ftvwine-500 hover:text-ftvwine-500 dark:text-ftvwine-500 dark:hover:text-ftvwine-400">
+                      <svg
+                        class="w-6 h-6"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path
+                          fill-rule="evenodd"
+                          d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="py-6 md:py-8">
+            <div class="gap-4 space-y-5 xl:flex xl:items-center xl:justify-between xl:space-y-0">
+              <a href="#" title="" class="block">
+                <img
+                  class="block h-8 w-auto dark:hidden"
+                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full.svg"
+                  alt=""
+                />
+                <img
+                  class="hidden h-8 w-auto dark:block"
+                  src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/logo-full-dark.svg"
+                  alt=""
+                />
+              </a>
+
+              <ul class="flex flex-wrap items-center gap-4 text-sm text-gray-900 dark:text-white xl:justify-center">
+                <li>
+                  <a href="#" title="" class="font-medium hover:underline">
+                    {" "}
+                    Legal Notice{" "}
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="" class="font-medium hover:underline">
+                    {" "}
+                    Assistant Onboarding Policy{" "}
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="" class="font-medium hover:underline">
+                    {" "}
+                    Terms of Use{" "}
+                  </a>
+                </li>
+              </ul>
+
+              <p class="text-sm text-ftvwine-25 dark:text-ftvwine-25">
+                © 2025{" "}
+                <a href="#" class="hover:underline">
+                  Innopower
+                </a>
+                , Inc. All rights reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
